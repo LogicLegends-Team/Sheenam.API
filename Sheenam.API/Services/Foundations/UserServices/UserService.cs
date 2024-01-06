483fe89d23287801407f56bdf7c5bced20a0864a
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2024-01-06 16:48:34 +0500

    POC: Crud Operators for User

---
+++ Sheenam.API/Services/Foundations/UserServices/UserService.cs
@@ -0,0 +1,35 @@
+﻿using Sheenam.API.Brokers.Storages;
+using Sheenam.API.Models.Users;
+
+namespace Sheenam.API.Services.Foundations.UserServices
+{
+    public class UserService : IUserService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public UserService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public async ValueTask<User> AddUserAsync(User user) =>
+            await this.storageBroker.InsertUserAsync(user);
+
+        public IQueryable<User> RetrieveAllUsers() =>
+            this.storageBroker.SelectAllUsers();
+
+        public async ValueTask<User> RetrieveUserByIdAsync(Guid id) =>
+            await this.storageBroker.SelectUserByIdAsync(id);
+
+        public async ValueTask<User> ModifyUserAsync(User user) =>
+            await this.storageBroker.UpdateUserAsync(user);
+
+        public async ValueTask<User> RemoveUserAsync(Guid id)
+        {
+            User maybeUser =
+                await this.storageBroker.SelectUserByIdAsync(id);
+
+            return await this.storageBroker.DeleteUserAsync(maybeUser);
+        }
+    }
+}
